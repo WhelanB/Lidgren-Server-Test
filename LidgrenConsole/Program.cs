de3046243218bccf3b8c5fef7935cd8776a41025
WhelanB <whelanb7@tcd.ie>
2016-03-14 21:48:41 +0000

    Source for a barebones server-client messaging system using the Lidgren Networking Library.

---
+++ LidgrenConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lidgren.Network;
+using System.Threading;
+
+namespace LidgrenConsole
+{
+    class Program
+    {
+        static NetClient client;
+        static NetPeerConfiguration config;
+        
+        static void Main(string[] args)
+        {
+            NetIncomingMessage chat;
+            config = new NetPeerConfiguration("Chat");
+            Console.WriteLine("Enter a Username:");
+            String username = Console.ReadLine();
+            client = new NetClient(config);
+            client.Start();
+            client.Connect("127.0.0.1", 25565);
+            Thread.Sleep(500);
+            
+            while (true)
+            {
+                NetOutgoingMessage msg = client.CreateMessage();
+                String text = Console.ReadLine();
+                msg.Write(username + ": " + text);
+                client.SendMessage(msg, NetDeliveryMethod.ReliableOrdered);
+               
+            }
+            
+            
+            client.Disconnect("bye bye");
+        }
+    }
+}
---
+++ LidgrenConsoleServer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lidgren.Network;
+
+namespace LidgrenConsoleServer
+{
+    class Program
+    {
+        static NetServer server;
+        static NetPeerConfiguration config;
+        static List<String> chatMessages;
+        static void Main(string[] args)
+        {
+            chatMessages = new List<String>();
+            config = new NetPeerConfiguration("Chat");
+            config.Port = 25565;
+            config.MaximumConnections = 10;
+            config.EnableMessageType(NetIncomingMessageType.ConnectionApproval);
+            server = new NetServer(config);
+            server.Start();
+            Console.WriteLine("Server has started");
+            NetIncomingMessage chat;
+            
+            while (true)
+            {
+                
+                if ((chat = server.ReadMessage()) != null)
+                {
+                    switch (chat.MessageType)
+                    {
+                        case NetIncomingMessageType.ConnectionApproval:
+                            chat.SenderConnection.Approve();
+                           // NetOutgoingMessage updateChat = server.CreateMessage();
+                           // updateChat.WriteAllProperties(chatMessages);
+                            // server.SendMessage(updateChat, chat.SenderConnection, NetDeliveryMethod.ReliableOrdered);
+                            break;
+                        case NetIncomingMessageType.StatusChanged:
+                            Console.WriteLine("User" + chat.SenderConnection.Status);
+                            break;
+                        case NetIncomingMessageType.Data:
+                            String chatMsg = chat.ReadString();
+                            Console.WriteLine(chatMsg);
+                            NetOutgoingMessage relayChat = server.CreateMessage();
+                            relayChat.Write(chatMsg);
+                            foreach(NetConnection c in server.Connections.Where(connection => connection != chat.SenderConnection ).ToList())
+                            {
+                                server.SendMessage(relayChat, c, NetDeliveryMethod.ReliableOrdered);
+                            }
+                          
+                            break;
+                      
+
+                    }
+                   
+
+                }
+            }
+            
+            
+        }
+    }
+}
